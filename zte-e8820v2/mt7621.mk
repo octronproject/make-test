47a0ce5324a20975cdb8ce3d244e14d48fa67f95
octron.project <octron.project@gmail.com>
2026-09-20 21:25:40 +0700

    Update mt7621.mk

--- zte-e8820v2/mt7621.mk
+++ zte-e8820v2/mt7621.mk
@@ -1650,10 +1650,11 @@ TARGET_DEVICES += zio_freezio
 
 define Device/zte_e8820v2
   $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820V2
-  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb3 kmod-usb-ledtrig-usbport wpad hostapd-utils luci
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb2 kmod-usb-ledtrig-usbport
 endef
 TARGET_DEVICES += zte_e8820v2
 
