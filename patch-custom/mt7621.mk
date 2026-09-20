4723de82dd65a8c3afbf298911492eddb732030e
octron.project <octron.project@gmail.com>
2026-09-20 17:11:05 +0700

    Update mt7621.mk

--- patch-custom/mt7621.mk
+++ patch-custom/mt7621.mk
@@ -4399,7 +4399,7 @@ define Device/zte_e8820v2
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820V2
-  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb2 kmod-usb-ledtrig-usbport
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb2 kmod-usb-ledtrig-usbport wpad hostapd-utils luci
 endef
 TARGET_DEVICES += zte_e8820v2
 
